6037e5d613c3359439832d14afa0e9e67af930c2
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-17 17:38:09 +0300

    Add functionality for playing one round

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * choices.length);
   return choices[randomNumber];
 }
+
+function playRound(playerSelection, computerSelection) {
+  const constPlayerSelection = playerSelection;
+  const constComputerSelection = computerSelection.toUpperCase();
+  if (constPlayerSelection == constComputerSelection) {
+    return `TIE YOU BOTH CHOSE ${constPlayerSelection}`;
+  } else if (
+    (constPlayerSelection == "ROCK" && constComputerSelection == "SCISSORS") ||
+    (constPlayerSelection == "PAPER" && constComputerSelection == "ROCK") ||
+    (constPlayerSelection == "SCISSORS" && constComputerSelection == "PAPER")
+  ) {
+    return `VICTORY! ${constPlayerSelection} BEATS ${constComputerSelection}`;
+  } else {
+    return `DEFEAT.. ${constComputerSelection} BEATS ${constPlayerSelection}`;
+  }
+}
+const playerSelection = "    roCk   ".trim().toUpperCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
