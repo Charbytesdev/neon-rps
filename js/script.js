af72703dd91b9e3e7e7668d98d78d3f86bfa9646
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-17 18:14:24 +0300

    Add functionality for playing a game of five rounds

--- js/script.js
+++ js/script.js
@@ -7,18 +7,38 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   const constPlayerSelection = playerSelection;
   const constComputerSelection = computerSelection.toUpperCase();
-  if (constPlayerSelection == constComputerSelection) {
-    return `TIE YOU BOTH CHOSE ${constPlayerSelection}`;
+  if (constPlayerSelection === constComputerSelection) {
+    alert(`TIE YOU BOTH CHOSE ${constPlayerSelection}`);
+    return "TIE";
   } else if (
-    (constPlayerSelection == "ROCK" && constComputerSelection == "SCISSORS") ||
-    (constPlayerSelection == "PAPER" && constComputerSelection == "ROCK") ||
-    (constPlayerSelection == "SCISSORS" && constComputerSelection == "PAPER")
+    (constPlayerSelection === "ROCK" &&
+      constComputerSelection === "SCISSORS") ||
+    (constPlayerSelection === "PAPER" && constComputerSelection === "ROCK") ||
+    (constPlayerSelection === "SCISSORS" && constComputerSelection === "PAPER")
   ) {
-    return `VICTORY! ${constPlayerSelection} BEATS ${constComputerSelection}`;
+    alert(`VICTORY! ${constPlayerSelection} BEATS ${constComputerSelection}`);
+    return "VICTORY";
   } else {
-    return `DEFEAT.. ${constComputerSelection} BEATS ${constPlayerSelection}`;
+    alert(`DEFEAT.. ${constComputerSelection} BEATS ${constPlayerSelection}`);
+    return "DEFEAT";
   }
 }
-const playerSelection = "    roCk   ".trim().toUpperCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let playerCount = 0;
+  let computerCount = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("ENTER CHOICE: ROCK, PAPER, SCISSORS?");
+    const formattedPlayerSelec = playerSelection.trim().toUpperCase();
+    const roundResult = playRound(formattedPlayerSelec, getComputerChoice());
+    if (roundResult === "VICTORY") playerCount++;
+    else if (roundResult === "DEFEAT") computerCount++;
+    else {
+      playerCount++;
+      computerCount++;
+    }
+    alert(`RESULTS: PLAYER: ${playerCount}, COMPUTER ${computerCount}`);
+  }
+}
+
+game();
